6563907a4f941b48dddc03607d58bcf1496ad7a8
miny <mksuuup@gmail.com>
2023-01-09 17:49:48 +0900

    ♻️ rename #3

--- python_code/good/single_responsibility.py
+++ python_code/good/single_responsibility.py
@@ -12,22 +12,22 @@ class IEmail(object):
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def setSender(self, sender):
+    def setsender(self, sender):
         pass
 
     @abstractmethod
-    def setReceiver(self, receiver):
+    def setreceiver(self, receiver):
         pass
 
     @abstractmethod
-    def setContent(self, content):
+    def setcontent(self, content):
         pass
 
 class IContent(object):
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def getString(self):
+    def getstring(self):
         pass
 
 class MyContent(IContent):
@@ -35,7 +35,7 @@ class MyContent(IContent):
     def __init__(self, content):
         self.content = content
 
-    def getString(self):
+    def getstring(self):
         return "<MyML>{}</MyML>".format(self.content)
 
 class Email(IEmail):
@@ -46,19 +46,19 @@ class Email(IEmail):
         self.__receiver = None
         self.__content = None
 
-    def setSender(self, sender):
+    def setsender(self, sender):
         if self.protocol == 'IM':
             self.__sender = ''.join(["I'm ", sender])
         else:
             self.__sender = sender
 
-    def setReceiver(self, receiver):
+    def setreceiver(self, receiver):
         if self.protocol == 'IM':
             self.__receiver = ''.join(["I'm ", receiver])
         else:
             self.__receiver = receiver
 
-    def setContent(self, content):
+    def setcontent(self, content):
         self.__content = content.getString()
 
     def __repr__(self):
@@ -69,7 +69,7 @@ class Email(IEmail):
 
 def main():
     email = Email('IM')
-    email.setSender('qmal')
+    email.setsender('qmal')
     email.setReceiver('james')
     content = MyContent('Hello, there!')
     email.setContent(content)
